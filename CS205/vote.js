0a466bb7eb63a20a882fbc8922a1a9195fd73cd1
gowri-web <gkgowri4@gmail.com>
2025-05-28 14:48:01 +0530

    hi

---
+++ CS205/vote.js
@@ -0,0 +1,10 @@
+function vote(){
+    var name=document.getElementById("name").value;
+    var age=document.getElementById("age").value;
+    var res=document.getElementById("res");
+    if(age>=18) {
+        res.innerHTML="you are eligible👍";
+    }else{
+        res.innerHTML="you are still a child 😆🤣";
+    }
+}
